0ebcc8b982322dcd2b70c49bdda2546a523bae7f
Vera Korzun <veraminich@gmail.com>
2021-02-11 13:23:06 +0100

    Added changes

--- src/page/home/HomePage.js
+++ src/page/home/HomePage.js
@@ -1,11 +1,47 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { getAuth } from "../../redux/selectors/authSelectors";
+import { mainRoutes } from "../../routes/mainRoutes";
 import HomePageStyled from "./HomePageStyled";
 
 const HomePage = () => {
-  //const isAuth = useSelector(getAuth);
+  const isAuth = useSelector(getAuth);
   return (
     <HomePageStyled>
       <h2 className="home-page__title">Welcome to the phonebook</h2>
+      <h3 className="home-page__title">Go to</h3>
+      <ul className="home__list">
+        {mainRoutes.map(({ name, path, exact, isPrivate, restricted }) => (
+          <>
+            {!isAuth && !isPrivate && restricted && (
+              <li className="home__list-item" key={path}>
+                <NavLink
+                  to={path}
+                  exact={exact}
+                  className="home__list-item-link"
+                  activeClassName="active-link"
+                >
+                  {name}
+                </NavLink>
+              </li>
+            )}
+            {isAuth && isPrivate && !restricted && (
+              <li className="home__list-item" key={path}>
+                <NavLink
+                  to={path}
+                  exact={exact}
+                  className="home__list-item-link"
+                  activeClassName="active-link"
+                >
+                  {name}
+                </NavLink>
+              </li>
+            )}
+          </>
+        ))}
+      </ul>
+      <h3 className="home-page__title">to work with application</h3>
     </HomePageStyled>
   );
 };
--- src/page/home/HomePageStyled.js
+++ src/page/home/HomePageStyled.js
@@ -2,12 +2,48 @@ import styled from "styled-components";
 
 const Wrapper = styled.div`
   .home-page__title {
-    font-weight: 700;
+    font-weight: 500;
     text-align: center;
     font-size: 25px;
     line-height: 1.63;
     color: #008080;
+  }
+  .home-page__title:not(:last-child) {
     margin-top: 50px;
+  }
+
+  .home__list {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    padding: 0;
+    margin: 0;
+    list-style: none;
+    margin-top: 15px;
+    margin-bottom: 15px;
+  }
+  .home__list-item {
+  }
+  .home__list-item:not(:last-child) {
+    margin-right: 15px;
+  }
+  .home__list-item-link {
+    font-weight: 500;
+    font-size: 25px;
+    line-height: 1.63;
+    color: #008080;
+    border: 1px solid #008080;
+    border-radius: 10px;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 10px 15px;
+  }
+  .home__list-item-link:hover {
+    color: red;
+    border: 1px solid red;
+  }
+  .active-link {
+    color: red;
   }
 `;
 
